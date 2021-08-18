c5dc216762473924a9fac7f75b686633d4adb530
Sultan Baimukhanov <portret008@gmail.com>
2021-08-18 14:10:29 +0600

    ts useffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
 import React from 'react';
 import './App.css';
+import GUsers from './GithubUsers/GUsers';
+import Rheader from './RocketUsplashApiF/Rcomponent/rheader-navbar';
 import RocketUnsplashApi from './RocketUsplashApiF/RocketUnsplashApi';
 
 
 function App() {
   return (
     <div className="App">
-      <RocketUnsplashApi />
+      <Rheader />
+      <GUsers />
+{/*       <RocketUnsplashApi /> */}
     </div>
 
   );
